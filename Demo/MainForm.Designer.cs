d2810a09e819bd0d0210253d1e80abf08b81d95b
Meister Eder <meistereder666@hotmail.de>
2017-10-16 11:25:20 +0200

    Projektdateien hinzufügen.

---
+++ Demo/MainForm.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace SingleTubeReader
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MainForm));
+            this.btnSave = new System.Windows.Forms.Button();
+            this.pictureBoxDisplay = new System.Windows.Forms.PictureBox();
+            this.comboBoxCameras = new System.Windows.Forms.ComboBox();
+            this.btnConfig = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtDecode = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.myNotifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label3 = new System.Windows.Forms.Label();
+            this.chkPreview = new System.Windows.Forms.CheckBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.picBMBH = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBMBH)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(108, 72);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(86, 23);
+            this.btnSave.TabIndex = 14;
+            this.btnSave.Text = "Bild speichern";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // pictureBoxDisplay
+            // 
+            this.pictureBoxDisplay.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBoxDisplay.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBoxDisplay.Location = new System.Drawing.Point(12, 12);
+            this.pictureBoxDisplay.Name = "pictureBoxDisplay";
+            this.pictureBoxDisplay.Size = new System.Drawing.Size(573, 475);
+            this.pictureBoxDisplay.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBoxDisplay.TabIndex = 13;
+            this.pictureBoxDisplay.TabStop = false;
+            // 
+            // comboBoxCameras
+            // 
+            this.comboBoxCameras.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBoxCameras.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxCameras.FormattingEnabled = true;
+            this.comboBoxCameras.Location = new System.Drawing.Point(642, 12);
+            this.comboBoxCameras.Name = "comboBoxCameras";
+            this.comboBoxCameras.Size = new System.Drawing.Size(153, 21);
+            this.comboBoxCameras.TabIndex = 12;
+            this.comboBoxCameras.SelectedIndexChanged += new System.EventHandler(this.comboBoxCameras_SelectedIndexChanged);
+            // 
+            // btnConfig
+            // 
+            this.btnConfig.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnConfig.Location = new System.Drawing.Point(12, 43);
+            this.btnConfig.Name = "btnConfig";
+            this.btnConfig.Size = new System.Drawing.Size(116, 23);
+            this.btnConfig.TabIndex = 10;
+            this.btnConfig.Text = "Kameraeinstellungen";
+            this.btnConfig.UseVisualStyleBackColor = true;
+            this.btnConfig.Click += new System.EventHandler(this.btnConfig_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Location = new System.Drawing.Point(65, 72);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(39, 23);
+            this.btnStop.TabIndex = 9;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.btnStop_Click);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(12, 72);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(47, 23);
+            this.btnStart.TabIndex = 8;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(596, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.TabIndex = 24;
+            this.label1.Text = "Gerät:";
+            // 
+            // txtDecode
+            // 
+            this.txtDecode.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtDecode.Enabled = false;
+            this.txtDecode.Location = new System.Drawing.Point(695, 39);
+            this.txtDecode.Name = "txtDecode";
+            this.txtDecode.Size = new System.Drawing.Size(100, 20);
+            this.txtDecode.TabIndex = 25;
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(596, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.TabIndex = 26;
+            this.label2.Text = "Dekodierergebnis:";
+            // 
+            // myNotifyIcon
+            // 
+            this.myNotifyIcon.BalloonTipText = "BMBH SingleTubeReader";
+            this.myNotifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("myNotifyIcon.Icon")));
+            this.myNotifyIcon.Text = "BMBH SingleTubeReader";
+            this.myNotifyIcon.Visible = true;
+            this.myNotifyIcon.Click += new System.EventHandler(this.myNotifyIcon_Click);
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.SystemColors.Control;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.label3.Location = new System.Drawing.Point(595, 356);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(204, 131);
+            this.label3.TabIndex = 27;
+            this.label3.Text = "BMBH SingleTubeReader              Copyright © 2017 Christoph Döllinger BioMateri" +
+    "alBank Heidelberg              Universitätsklinikum Heidelberg";
+            // 
+            // chkPreview
+            // 
+            this.chkPreview.AutoSize = true;
+            this.chkPreview.Location = new System.Drawing.Point(12, 20);
+            this.chkPreview.Name = "chkPreview";
+            this.chkPreview.Size = new System.Drawing.Size(117, 17);
+            this.chkPreview.TabIndex = 29;
+            this.chkPreview.Text = "Vorschau anzeigen";
+            this.chkPreview.UseVisualStyleBackColor = true;
+            this.chkPreview.CheckedChanged += new System.EventHandler(this.chkPreview_CheckedChanged);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnSave);
+            this.groupBox1.Controls.Add(this.chkPreview);
+            this.groupBox1.Controls.Add(this.btnStop);
+            this.groupBox1.Controls.Add(this.btnStart);
+            this.groupBox1.Controls.Add(this.btnConfig);
+            this.groupBox1.Location = new System.Drawing.Point(595, 75);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(200, 109);
+            this.groupBox1.TabIndex = 32;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Einstellungen";
+            // 
+            // picBMBH
+            // 
+            this.picBMBH.BackColor = System.Drawing.SystemColors.Control;
+            this.picBMBH.Image = ((System.Drawing.Image)(resources.GetObject("picBMBH.Image")));
+            this.picBMBH.Location = new System.Drawing.Point(602, 415);
+            this.picBMBH.Name = "picBMBH";
+            this.picBMBH.Size = new System.Drawing.Size(168, 65);
+            this.picBMBH.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.picBMBH.TabIndex = 33;
+            this.picBMBH.TabStop = false;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CausesValidation = false;
+            this.ClientSize = new System.Drawing.Size(807, 499);
+            this.Controls.Add(this.picBMBH);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtDecode);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBoxDisplay);
+            this.Controls.Add(this.comboBoxCameras);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimumSize = new System.Drawing.Size(640, 520);
+            this.Name = "MainForm";
+            this.Text = "BMBH SingleTubeReader";
+            this.TopMost = true;
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxDisplay)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.picBMBH)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.PictureBox pictureBoxDisplay;
+        private System.Windows.Forms.ComboBox comboBoxCameras;
+        private System.Windows.Forms.Button btnConfig;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtDecode;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NotifyIcon myNotifyIcon;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.CheckBox chkPreview;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox picBMBH;
+    }
+}
+
---
+++ Demo/MainForm.cs
@@ -0,0 +1,406 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Media;
+using Touchless.Vision.Camera;
+using DataMatrix.net;
+using System.Drawing.Imaging;
+using System.Threading;
+using System.Reflection;
+
+namespace SingleTubeReader
+{
+    public partial class MainForm : Form
+    {
+        Thread t;
+        long isRun = 0L;
+        string lastBarcode = "";
+        object lastBC_lock = new object();
+        bool init = false;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.WindowState = FormWindowState.Minimized;
+            this.ShowInTaskbar = false;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            if (!DesignMode)
+            {
+                // Refresh the list of available cameras
+                comboBoxCameras.Items.Clear();
+                int i = 0;
+                int nScanCam = 0;
+                foreach (Camera cam in CameraService.AvailableCameras)
+                {
+                    comboBoxCameras.Items.Add(cam);
+                    if (cam.Name == "USB2.0 PC CAMERA")
+                        nScanCam = i;
+                    ++i;
+                }
+
+                if (comboBoxCameras.Items.Count > 0)
+                {
+                    comboBoxCameras.SelectedIndex = nScanCam;
+                    btnStart_Click(sender, e);
+                    StartStopThread();
+                }
+                init = true;
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            StartStopThread();
+            thrashOldCamera();
+        }
+
+        private void btnStop_Click(object sender, EventArgs e)
+        {
+            StartStopThread();
+            thrashOldCamera();
+        }
+
+        private CameraFrameSource _frameSource;
+        private static Bitmap _latestFrame;
+        
+        private Camera CurrentCamera
+        {
+            get
+            {
+                return comboBoxCameras.SelectedItem as Camera;
+            }
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            // Early return if we've selected the current camera
+            if (_frameSource != null && _frameSource.Camera == comboBoxCameras.SelectedItem)
+                return;
+            thrashOldCamera();
+            startCapturing();
+        }
+
+        private void startCapturing()
+        {
+            try
+            {
+                Camera c = (Camera)comboBoxCameras.SelectedItem;
+                setFrameSource(new CameraFrameSource(c));
+                _frameSource.Camera.CaptureWidth = 640;
+                _frameSource.Camera.CaptureHeight = 480;
+                _frameSource.Camera.Fps = 50;
+                _frameSource.NewFrame += OnImageCaptured;
+
+                if(chkPreview.Checked)
+                    pictureBoxDisplay.Paint += new PaintEventHandler(drawLatestImage);
+
+                _frameSource.StartFrameCapture();
+            }
+            catch (Exception ex)
+            {
+                comboBoxCameras.Text = "Select A Camera";
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void drawLatestImage(object sender, PaintEventArgs e)
+        {
+            if (_latestFrame != null)
+            {
+                // Draw the latest image from the active camera
+                //_latestFrame = ResizeImage(_latestFrame, new Size(320, 240));
+                e.Graphics.DrawImage(_latestFrame, 0, 0, _latestFrame.Width, _latestFrame.Height);
+            }
+        }
+
+        public void OnImageCaptured(Touchless.Vision.Contracts.IFrameSource frameSource, Touchless.Vision.Contracts.Frame frame, double fps)
+        {
+            _latestFrame = frame.Image;
+
+            if (chkPreview.Checked)
+                pictureBoxDisplay.Invalidate();
+        }
+
+        private void setFrameSource(CameraFrameSource cameraFrameSource)
+        {
+            if (_frameSource == cameraFrameSource)
+                return;
+
+            _frameSource = cameraFrameSource;
+        }
+
+        //
+
+        private void thrashOldCamera()
+        {
+            // Trash the old camera
+            if (_frameSource != null)
+            {
+                _frameSource.NewFrame -= OnImageCaptured;
+                _frameSource.Camera.Dispose();
+                setFrameSource(null);
+
+                if (chkPreview.Checked)
+                    pictureBoxDisplay.Paint -= new PaintEventHandler(drawLatestImage);
+            }
+        }
+
+        //
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            if (_frameSource == null)
+                return;
+
+            Bitmap current = (Bitmap)_latestFrame.Clone();
+            using (SaveFileDialog sfd = new SaveFileDialog())
+            {
+                sfd.Filter = "*.png|*.png";
+                if (sfd.ShowDialog() == DialogResult.OK)
+                {
+                    current.Save(sfd.FileName);
+                }
+            }
+
+            current.Dispose();
+        }
+
+        private void btnConfig_Click(object sender, EventArgs e)
+        {
+            // snap camera
+            if (_frameSource != null)
+                _frameSource.Camera.ShowPropertiesDialog();
+        }
+
+        #region Camera Property Controls
+        private IDictionary<String, CameraProperty> displayPropertyValues;
+
+        private IDictionary<String, CameraProperty> DisplayPropertyValues
+        {
+            get
+            {
+                if (displayPropertyValues == null)
+                    displayPropertyValues = new Dictionary<String, CameraProperty>()
+                 {
+                    { "Pan (Degrees)", CameraProperty.Pan_degrees },
+                    { "Tilt (Degrees)", CameraProperty.Tilt_degrees },
+                    { "Roll (Degrees)", CameraProperty.Roll_degrees },
+                    { "Zoom (mm)", CameraProperty.Zoom_mm },
+                    { "Exposure (log2(seconds))", CameraProperty.Exposure_lgSec },
+                    { "Iris (10f)", CameraProperty.Iris_10f },
+                    { "Focal Length (mm)", CameraProperty.FocalLength_mm },
+                    { "Flash", CameraProperty.Flash },
+                    { "Brightness", CameraProperty.Brightness },
+                    { "Contrast", CameraProperty.Contrast },
+                    { "Hue", CameraProperty.Hue },
+                    { "Saturation", CameraProperty.Saturation },
+                    { "Sharpness", CameraProperty.Sharpness },
+                    { "Gamma", CameraProperty.Gamma },
+                    { "Color Enable", CameraProperty.ColorEnable },
+                    { "White Balance", CameraProperty.WhiteBalance },
+                    { "Backlight Compensation", CameraProperty.BacklightCompensation },
+                    { "Gain", CameraProperty.Gain },
+                 };
+
+                return displayPropertyValues;
+            }
+        }
+
+        private IDictionary<CameraProperty, CameraPropertyCapabilities> CurrentCameraPropertyCapabilities
+        {
+            get;
+            set;
+        }
+
+        private IDictionary<CameraProperty, CameraPropertyRange> CurrentCameraPropertyRanges
+        {
+            get;
+            set;
+        }
+
+        private Boolean IsSelectedCameraPropertySupported
+        {
+            get;
+            set;
+        }
+
+        private Boolean SuppressCameraPropertyValueValueChangedEvent
+        {
+            get;
+            set;
+        }
+
+        private Boolean CameraPropertyControlInitializationComplete
+        {
+            get;
+            set;
+        }
+
+
+
+        #endregion
+
+        private void SystemBell()
+        {
+            SystemSounds.Asterisk.Play();
+        }
+
+        private delegate void SetControlPropertyThreadSafeDelegate(
+           Control control,
+           string propertyName,
+           object propertyValue);
+
+        public static void SetControlPropertyThreadSafe(
+            Control control,
+            string propertyName,
+            object propertyValue)
+        {
+            if (control.InvokeRequired)
+            {
+                control.Invoke(new SetControlPropertyThreadSafeDelegate
+                (SetControlPropertyThreadSafe),
+                new object[] { control, propertyName, propertyValue });
+            }
+            else
+            {
+                control.GetType().InvokeMember(
+                    propertyName,
+                    BindingFlags.SetProperty,
+                    null,
+                    control,
+                    new object[] { propertyValue });
+            }
+        }
+
+        //public static Bitmap ResizeImage(Bitmap imgToResize, Size size)
+        //{
+        //    try
+        //    {
+        //        Bitmap b = new Bitmap(size.Width, size.Height);
+        //        using (Graphics g = Graphics.FromImage((Image)b))
+        //        {
+        //            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.Low;
+        //            g.DrawImage(imgToResize,
+        //                        new Rectangle(0, 0, size.Width, size.Height),
+        //                        new Rectangle(64, 48, 448, 336),
+        //                        GraphicsUnit.Pixel);
+        //        }
+        //        return b;
+        //    }
+        //    catch
+        //    {
+        //        Console.WriteLine("Bitmap could not be resized");
+        //        return imgToResize;
+        //    }
+        //}
+
+        private string Decode()
+        {
+            DmtxImageDecoder decoder = new DmtxImageDecoder();
+         
+            string code = "";
+
+            try
+            {
+                //Bitmap current = ResizeImage((Bitmap)_latestFrame.Clone(), new Size(320, 240));
+                Bitmap current = (Bitmap)_latestFrame.Clone();
+                List<string> codes = decoder.DecodeImage(current, 1, new TimeSpan(0, 0, 0, 0, 800));
+
+                if (codes.Count > 0)
+                    code = codes[0] + "\r\n";
+            }
+            catch { }
+ 
+            return code;
+        }
+
+        private void DecodeThread()
+        {
+            string _out = "";
+
+            do
+            {
+                _out = Decode();
+
+                if (lastBarcode != _out && _out.Length > 1)
+                {
+                    Monitor.Enter(lastBC_lock);
+                    lastBarcode = _out;
+                    Monitor.Exit(lastBC_lock);
+
+                    // thread-safe equivalent of myLabel.Text = status;
+                    SetControlPropertyThreadSafe(txtDecode, "Text", lastBarcode);
+                    SystemBell();
+                    SendKeys.SendWait(lastBarcode);
+                }
+
+                Thread.Sleep(500);
+            }
+            while (Interlocked.Read(ref isRun) == 1L);
+        }
+
+        private void StartStopThread()
+        {
+            if (Interlocked.Read(ref isRun) == 0L)
+            {
+                Interlocked.Increment(ref isRun);
+                t = new Thread(DecodeThread);
+                t.IsBackground = true;
+                t.Start();
+            }
+            else
+            {
+                Interlocked.Exchange(ref isRun, 0L); // stop thread
+                t.Join(); // wait for end
+                //MessageBox.Show("Decoding stopped");
+            }
+        }
+        
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            if (FormWindowState.Minimized == this.WindowState)
+            {
+                myNotifyIcon.Visible = true;
+                myNotifyIcon.ShowBalloonTip(500);
+                this.Hide();
+            }
+
+            else if (FormWindowState.Normal == this.WindowState)
+            {
+                myNotifyIcon.Visible = false;
+            }
+        }
+
+        private void myNotifyIcon_Click(object sender, EventArgs e)
+        {
+            this.Show();
+            this.WindowState = FormWindowState.Normal;
+            this.ShowInTaskbar = true;
+        }
+
+        private void comboBoxCameras_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (init)
+            {
+                StartStopThread();
+                thrashOldCamera();
+                btnStart_Click(sender, e);
+                StartStopThread();
+            }
+        }
+
+        private void chkPreview_CheckedChanged(object sender, EventArgs e)
+        {
+            thrashOldCamera();
+            startCapturing();
+        }
+    }
+}
